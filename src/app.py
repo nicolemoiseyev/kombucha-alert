aadde29c67bb2a3aeb7ab760e4188756ec3ab757
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2020-06-13 16:09:53 -0400

    Modularize app.py

--- src/app.py
+++ src/app.py
@@ -10,73 +10,80 @@ from selenium.webdriver.support import expected_conditions as EC
 # Load config
 config = yaml.safe_load(open("./config.yml"))
 
-'''
-Get available product info
-'''
-
 # product we want to search for on whole foods
 search_query = "GT Kombucha"
 product_name = "GT's, Organic Raw Kombucha Lemonade, 16.2 Ounce"
 
-# Run the driver in headless mode on a server
-options = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
-driver = webdriver.Chrome(options=options,executable_path=config["CHROME_DRIVER_PATH"])
+# parameters to send the notification through slack
+SLACK_TOKEN = config["SLACK_TOKEN"]
+SLACK_CHANNEL = "#lemonade"
 
-#driver = webdriver.Chrome(executable_path=config["CHROME_DRIVER_PATH"])
-driver.get("https://primenow.amazon.com/")
 
-# Locate the zipcode input field and enter the appropriate store zipcode
-zipcode = driver.find_element_by_id("lsPostalCode")
-zipcode.send_keys(config["ZIPCODE"])
-submit_zipcode = driver.find_element_by_id("a-autoid-1").click()
+'''
+Get available products list
+'''
+def get_products(search_query):
+    # Run the driver in headless mode on a server
+    options = Options()
+    options.headless = True
+    options.add_argument("--window-size=1920,1200")
+    driver = webdriver.Chrome(options=options,executable_path=config["CHROME_DRIVER_PATH"])
 
-# wait up to 5s until the following page loads with the product search field
-wait = WebDriverWait(driver, 5)
-product_search = wait.until(
-    EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search Whole Foods Market']")))
+    #driver = webdriver.Chrome(executable_path=config["CHROME_DRIVER_PATH"])
+    driver.get("https://primenow.amazon.com/")
 
-# Enter the product and submit the search
-product_search.send_keys(search_query)
-submit_search = driver.find_element_by_xpath(
-    "//button[@class='page_header_search_wrapper__searchButton__2Vo9i']").click()
+    # Locate the zipcode input field and enter the appropriate store zipcode
+    zipcode = driver.find_element_by_id("lsPostalCode")
+    zipcode.send_keys(config["ZIPCODE"])
+    submit_zipcode = driver.find_element_by_id("a-autoid-1").click()
 
-# get the list of products after waiting up to 5s for the search to load
-# we're assuming the product we want is on the first page
-products_list = wait.until(
-    EC.presence_of_element_located((By.XPATH, "//ul[@class='product_grid__grid__1CpnV']/li")))
+    # wait up to 5s until the following page loads with the product search field
+    wait = WebDriverWait(driver, 5)
+    product_search = wait.until(
+        EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search Whole Foods Market']")))
 
-# iterate over existing products
-available_products = []
-for item in driver.find_elements_by_class_name('asin_card__title__1Oc6S'): # class of elements corresponding to product names
-    item_name = item.find_element_by_xpath('.//div/div').text
-    available_products.append(item_name)
+    # Enter the product and submit the search
+    product_search.send_keys(search_query)
+    submit_search = driver.find_element_by_xpath(
+        "//button[@class='page_header_search_wrapper__searchButton__2Vo9i']").click()
 
-def product_is_available(product):
+    # get the list of products after waiting up to 5s for the search to load
+    # we're assuming the product we want is on the first page
+    products_list = wait.until(
+        EC.presence_of_element_located((By.XPATH, "//ul[@class='product_grid__grid__1CpnV']/li")))
+
+    # iterate over existing products
+    available_products = []
+    for item in driver.find_elements_by_class_name('asin_card__title__1Oc6S'): # class of elements corresponding to product names
+        item_name = item.find_element_by_xpath('.//div/div').text
+        available_products.append(item_name)
+
+    return available_products
+
+def product_is_available(product, available_products):
     return product in available_products
 
 
 '''
 Send notification through slack
 '''
+def send_notification(product_name, available_products)
+    if product_is_available(product_name):
+        message = f"*{product_name}* is available on Whole Foods PrimeNow!"
+    else:
+        message = "Product not available"
 
-# parameters to send the notification through slack
-SLACK_TOKEN = config["SLACK_TOKEN"]
-SLACK_CHANNEL = "#lemonade"
+    params = {
+        "token": SLACK_TOKEN,
+        "channel": SLACK_CHANNEL,
+        "text": message
+    }
+    slack_endpt = "https://slack.com/api/chat.postMessage"
+    message = requests.post(slack_endpt, params = params)
 
-if product_is_available(product_name):
-    message = f"*{product_name}* is available on Whole Foods PrimeNow!"
-else:
-    message = "Product not available"
+    return message
 
-params = {
-    "token": SLACK_TOKEN,
-    "channel": SLACK_CHANNEL,
-    "text": message
-}
-
-slack_endpt = "https://slack.com/api/chat.postMessage"
-message = requests.post(slack_endpt, params = params)
-
-print(message.json())
+if __name__ == "__main__":
+    available_products = get_products(search_query)
+    message = send_notification(product_name, available_products)
+    print(message)
